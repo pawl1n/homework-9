a86c92ec5ae16bfe5f2796b67e3891e4e0d447a5
pawl1n <pinchuk.andryusha310212@gmail.com>
2022-11-06 18:46:00 +0200

    feat: MyStack class

--- src/main/java/stack/Main.java
+++ src/main/java/stack/Main.java
@@ -2,5 +2,14 @@ package stack;
 
 public class Main {
     public static void main(String[] args) {
+        MyStack<String> stack = new MyStack<>();
+        stack.push("first string");
+        stack.push("second string");
+        stack.push("third string");
+        stack.push("fourth string");
+        stack.push("fifth string");
+        stack.push("sixth string");
+        stack.push("seventh string");
+        System.out.println(stack);
     }
 }
